5e01062f23d720b81e9bb8d52fb6c5b4330705ed
vkoll29 <awino_g@outlook.com>
2022-02-23 07:48:26 +0300

    Finished with decorators

---
+++ selenium/main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get('https://www.python.org')
+assert "Python" in driver.title
+el = driver.find_element_by_name('q')
+el.clear()
+el.send_keys('pycon')
+el.send_keys(Keys.RETURN)
+assert "No Results Found" not in driver.page_source
+driver.close()
